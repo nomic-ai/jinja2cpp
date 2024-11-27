997ee7bdafab32a2c93b6f664810f6c24e953ea8
Adam Treat <treat.adam@gmail.com>
2024-11-27 10:43:13 -0500

    revert the install override and try exclude from all

--- thirdparty/internal_deps.cmake
+++ thirdparty/internal_deps.cmake
@@ -45,19 +45,14 @@ set (RAPIDJSON_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set (RAPIDJSON_BUILD_THIRDPARTY_GTEST OFF CACHE BOOL "" FORCE)
 set (RAPIDJSON_ENABLE_INSTRUMENTATION_OPT OFF CACHE BOOL "" FORCE)
 
-function(install)
-    # Do nothing; disable install commands for RapidJSON
-endfunction()
-
 FetchContent_Declare(
     rapidjson
     #    GIT_TAG f9d53419e912910fd8fa57d5705fa41425428c35 - latest but broken revision
     URL https://github.com/Tencent/rapidjson/archive/973dc9c06dcd3d035ebd039cfb9ea457721ec213.tar.gz
     URL_HASH SHA256=d0c9e52823d493206eb721d38cb3a669ca0212360862bd15a3c2f7d35ea7c6f7
 )
+set(FETCHCONTENT_SOURCE_DIR_RAPIDJSON EXCLUDE_FROM_ALL)
 FetchContent_MakeAvailable(rapidjson)
-
-unset(install)
 
 find_package(RapidJSON REQUIRED
              PATHS "${rapidjson_BINARY_DIR}"
